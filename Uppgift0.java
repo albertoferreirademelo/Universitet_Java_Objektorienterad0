da85de4ee60e9fbef8f021cc5adfb01e160379be
Alberto Ferreira de Melo <73712121+albertoferreirademelo@users.noreply.github.com>
2022-08-29 13:55:56 +0200

    adding method description

--- Uppgift0.java
+++ Uppgift0.java
@@ -1,3 +1,9 @@
+/**
+ * Print out hej värld!
+ * @author Alberto Ferreira, albfer-2
+
+*/
+
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hej värld!");        
